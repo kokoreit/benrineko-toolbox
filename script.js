7641908e2b7202164982ab4feeed735c55c0d596
kokoreit <128270978+kokoreit@users.noreply.github.com>
2023-04-26 22:43:24 +0900

    Add files via upload
    ヘッダーとフッターをつけてリファクタリングしました。

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
   section.scrollIntoView({ behavior: 'smooth' });
+  /*
+  本当はHTML（a href="#section1"）のみで実現可能ですが、
+  下記の理由でJSを使用します。
+  ↓
+  古いブラウザではスムーズスクロールをサポートしていないため。
+  */
 }
