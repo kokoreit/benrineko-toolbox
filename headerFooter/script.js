2129d23d6ef4c542a8104dac84521f61770a8421
kokoreit <128270978+kokoreit@users.noreply.github.com>
2023-05-19 17:28:22 +0900

    Add files via upload
    ロードされてからJSのイベントを追加する仕様にしました。
    これでHTMLのHEADで読み込んでも動作します。

--- headerFooter/script.js
+++ headerFooter/script.js
@@ -1,18 +1,32 @@
-const menuButton = document.querySelector('#menu-button');
-const menuContents = document.querySelector('#menu-contents');
+//ロードが完了されてからイベントを追加する
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.querySelector('#menu-button');
+  const menuContents = document.querySelector('#menu-contents');
 
-menuButton.addEventListener('click', function() {
-  menuContents.classList.toggle('show');
-  menuButton.classList.toggle('opened');
-});
-
-document.addEventListener('click', function(event) {
-  if (!menuButton.contains(event.target) && !menuContents.contains(event.target)) {
-    menuContents.classList.remove('show');
-    menuButton.classList.remove('opened');
+  //メニューのオンオフ
+  function toggleMenu() {
+    menuContents.classList.toggle('show');
+    menuButton.classList.toggle('opened');
   }
+  menuButton.addEventListener('click', toggleMenu);
+  
+  //メニューを閉じる
+  function closeMenu(event) {
+    if (!menuButton.contains(event.target) && !menuContents.contains(event.target)) {
+      menuContents.classList.remove('show');
+      menuButton.classList.remove('opened');
+    }
+  }
+  document.addEventListener('click', closeMenu);
+
+  // イベントリスナーを解除
+  function removeEventListeners() {
+    menuButton.removeEventListener('click', toggleMenu);
+    document.removeEventListener('click', closeMenu);
+  }
+  window.addEventListener('unload', removeEventListeners);
 });
 
-/*
-メモリリークok
-*/
+// /*
+// メモリリークok
+// */
