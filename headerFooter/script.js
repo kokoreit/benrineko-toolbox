5074d66e29e1eeb9c065a99366d0a7b7c8303c28
kokoreit <128270978+kokoreit@users.noreply.github.com>
2023-05-19 17:37:33 +0900

    Add files via upload
    先程のアップデート（ロード後にイベントを追加するコード）は意味がなかったので元に戻しました。

--- headerFooter/script.js
+++ headerFooter/script.js
@@ -1,32 +1,18 @@
-//ロードが完了されてからイベントを追加する
-document.addEventListener('DOMContentLoaded', function() {
-  const menuButton = document.querySelector('#menu-button');
-  const menuContents = document.querySelector('#menu-contents');
+const menuButton = document.querySelector('#menu-button');
+const menuContents = document.querySelector('#menu-contents');
 
-  //メニューのオンオフ
-  function toggleMenu() {
-    menuContents.classList.toggle('show');
-    menuButton.classList.toggle('opened');
-  }
-  menuButton.addEventListener('click', toggleMenu);
-  
-  //メニューを閉じる
-  function closeMenu(event) {
-    if (!menuButton.contains(event.target) && !menuContents.contains(event.target)) {
-      menuContents.classList.remove('show');
-      menuButton.classList.remove('opened');
-    }
-  }
-  document.addEventListener('click', closeMenu);
-
-  // イベントリスナーを解除
-  function removeEventListeners() {
-    menuButton.removeEventListener('click', toggleMenu);
-    document.removeEventListener('click', closeMenu);
-  }
-  window.addEventListener('unload', removeEventListeners);
+menuButton.addEventListener('click', function() {
+  menuContents.classList.toggle('show');
+  menuButton.classList.toggle('opened');
 });
 
-// /*
-// メモリリークok
-// */
+document.addEventListener('click', function(event) {
+  if (!menuButton.contains(event.target) && !menuContents.contains(event.target)) {
+    menuContents.classList.remove('show');
+    menuButton.classList.remove('opened');
+  }
+});
+
+/*
+メモリリークok
+*/
