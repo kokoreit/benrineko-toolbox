aeddce62e163f8534bc1d1af1db6bc5d5b0a5e6b
kokoreit <kokoreit@example.com>
2023-05-24 15:44:07 +0900

    ヘッダーフッター：JSでヘッダーメニューコンテンツを置き換える仕様になりました。クーポンお得計算機・フォントサイズ変換機を対応させました。

--- headerFooter/script.js
+++ headerFooter/script.js
@@ -1,7 +1,27 @@
-const headerMenuButton = document.querySelector("#header-menu-button");
 const headerMenuContents = document.querySelector("#header-menu-contents");
+const headerMenuButton = document.querySelector("#header-menu-button");
 const overlay = document.querySelector(".overlay");
 
+/*========= ヘッダー用のメニューをコードから追加===============*/
+var headerMenuTemplate = `
+        <p>生活</p>
+        <a href="../couponotoku/index.html">クーポンお得計算機</a>
+        <a href="../natureSounds/index.html">自然音（環境音）</a>
+        <a href="../nengouhenkan/index.html">年号変換</a>
+        <p>開発</p>
+        <a href="../dollarExtractor/index.html">$変数コピペツール</a>
+        <a href="../hashExtractor/index.html">カラーコード抽出機</a>
+        <a href="../fontsizeConverter/index.html">フォントサイズ単位変換</a>
+`;
+document.addEventListener('DOMContentLoaded', function() {
+  headerMenuContents.innerHTML = headerMenuTemplate;
+  /* 
+  ここでは可変コンテンツを追加します。
+  外部入力を受け取らないため、エスケープ処理は不要です。
+  */
+});
+
+/*========= ヘッダーの動作（HTMLに記述してある要素を使う） ===============*/
 headerMenuButton.addEventListener("click", function () {
   headerMenuContents.classList.toggle("show");
   updateMenuState();
