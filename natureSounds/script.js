0ed4a9b8adbb8edfe6a31556be669d8cedecc0cf
kokoreit <kokoreit@example.com>
2023-06-08 18:50:09 +0900

    自然音：スクロール画像処理をコメントアウトしました。

--- natureSounds/script.js
+++ natureSounds/script.js
@@ -62,91 +62,87 @@ window.addEventListener('load', function() {
 （ロードイベントやスクロールアクションも同様に無効です。）
 */
 
-/*========= 背景イメージの処理 ===============*/
-const bkImg = document.querySelector('.bkImg');
-const initialOffset = bkImg.offsetTop;
-const initialPosition = bkImg.style.position;
-const initialTop = bkImg.style.top;
-let animationId; // アニメーションのIDを保持する変数
-let isScrolling = false; // スクロール中かどうかを示すフラグ
-let scrollTimeout; // スクロール停止のタイムアウトIDを保持する変数
+// /*========= 背景イメージの処理 ===============*/
+// const bkImg = document.querySelector('.bkImg');
+// const initialOffset = bkImg.offsetTop;
+// const initialPosition = bkImg.style.position;
+// const initialTop = bkImg.style.top;
+// let animationId; // アニメーションのIDを保持する変数
+// let isScrolling = false; // スクロール中かどうかを示すフラグ
+// let scrollTimeout; // スクロール停止のタイムアウトIDを保持する変数
 
-// 背景位置の更新
-function updatePosition() {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  // bkImg.style.transform = `translateY(${initialOffset - scrollTop * 0.3}px)`;
-  bkImg.style.transform = `translate3d(0, ${initialOffset - scrollTop * 0.1}px, 0)`;
-  /*
-  【topよりtransform】
-  ・GPUを使用するので、より効率的にアニメーションを描画・スムーズな動きが実現されます。
-  ・要素を独立した合成レイヤーに配置、最適化、背景画像の移動がスムーズになります。
-  ・top（レイアウト計算）：相対的にコストが高く、アニメーションがスムーズになりにくい。
-  ・translateY() 関数はハードウェアアクセラレーションによる最適化を受けやすく、高速でスムーズなアニメーションを実現できます。
-  */
-}
-// アニメーションループ
-function animate() {
-  updatePosition();
-  animationId = requestAnimationFrame(animate);//予約
-  /*
-  【requestAnimationFrameがパフォーマンスが良い理由】
-  ブラウザは最適なタイミングでアニメーションを実行します。
-  ブラウザやタブが非表示の時、アニメーションは一時停止されます。
- */
-}
-// アニメーションの開始
-function startAnimation() {
-  if (!animationId && isScrolling) {
-    animationId = requestAnimationFrame(animate);//予約
-  }
-}
-// アニメーションの停止
-function stopAnimation() {
-  if (animationId) {
-    cancelAnimationFrame(animationId);
-    animationId = undefined;
-  }
-}
-// スクロールイベントを処理する
-function handleScroll() {
-  //アニメーションループスタート
-  isScrolling = true;
-  startAnimation();
-  //時間が経ったらアニメーションループを停止する
-  clearTimeout(scrollTimeout);　// タイマーの削除
-  scrollTimeout = setTimeout(function() {
-    isScrolling = false;
-    stopAnimation();
-  }, 200);
-}
-// 背景位置のリセット
-function resetPosition() {
-  bkImg.style.position = initialPosition;
-  bkImg.style.transform = 'none';
-  updatePosition();
-}
-// クリーンアップ
-function cleanup() {
-  stopAnimation();
-  clearTimeout(scrollTimeout); // タイマーの削除
-  window.removeEventListener('scroll', handleScroll);
-  window.removeEventListener('resize', resetPosition);
-  window.removeEventListener('beforeunload', cleanup);
-}
-// スペックが悪い端末ではイベントリスナーを登録しない
-const deviceMemory = navigator.deviceMemory;
-const cpuClass = navigator.cpuClass;
-const MIN_DEVICE_MEMORY = 4;
-const MIN_CPU_CLASS = 500;
-if (deviceMemory >= MIN_DEVICE_MEMORY && cpuClass >= MIN_CPU_CLASS) {
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('resize', resetPosition);
-  window.addEventListener('beforeunload', cleanup);
-  startAnimation();
-}
+// // 背景位置の更新
+// function updatePosition() {
+//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   // bkImg.style.transform = `translateY(${initialOffset - scrollTop * 0.3}px)`;
+//   bkImg.style.transform = `translate3d(0, ${initialOffset - scrollTop * 0.3}px, 0)`;
+//   /*
+//   【topよりtransform】
+//   ・GPUを使用するので、より効率的にアニメーションを描画・スムーズな動きが実現されます。
+//   ・要素を独立した合成レイヤーに配置、最適化、背景画像の移動がスムーズになります。
+//   ・top（レイアウト計算）：相対的にコストが高く、アニメーションがスムーズになりにくい。
+//   ・translateY() 関数はハードウェアアクセラレーションによる最適化を受けやすく、高速でスムーズなアニメーションを実現できます。
+//   */
+// }
+// // アニメーションループ
+// function animate() {
+//   updatePosition();
+//   animationId = requestAnimationFrame(animate);//予約
+//   /*
+//   【requestAnimationFrameがパフォーマンスが良い理由】
+//   ブラウザは最適なタイミングでアニメーションを実行します。
+//   ブラウザやタブが非表示の時、アニメーションは一時停止されます。
+//  */
+// }
+// // アニメーションの開始
+// function startAnimation() {
+//   if (!animationId && isScrolling) {
+//     animationId = requestAnimationFrame(animate);//予約
+//   }
+// }
+// // アニメーションの停止
+// function stopAnimation() {
+//   if (animationId) {
+//     cancelAnimationFrame(animationId);
+//     animationId = undefined;
+//   }
+// }
+// // スクロールイベントを処理する
+// function handleScroll() {
+//   //アニメーションループスタート
+//   isScrolling = true;
+//   startAnimation();
+//   //時間が経ったらアニメーションループを停止する
+//   clearTimeout(scrollTimeout);　// タイマーの削除
+//   scrollTimeout = setTimeout(function() {
+//     isScrolling = false;
+//     stopAnimation();
+//   }, 200);
+// }
+// // 背景位置のリセット
+// function resetPosition() {
+//   bkImg.style.position = initialPosition;
+//   bkImg.style.transform = 'none';
+//   updatePosition();
+// }
+// // クリーンアップ
+// function cleanup() {
+//   stopAnimation();
+//   clearTimeout(scrollTimeout); // タイマーの削除
+//   window.removeEventListener('scroll', handleScroll);
+//   window.removeEventListener('resize', resetPosition);
+//   window.removeEventListener('beforeunload', cleanup);
+// }
+// window.addEventListener('scroll', handleScroll);
+// window.addEventListener('resize', resetPosition);
+// window.addEventListener('beforeunload', cleanup);
+// startAnimation();
 
-/*========= メモ ===============*/
 /*
-【AI判定】
-メモリリークOK
+【背景処理のコメントアウトの理由】
+スペックが低い端末ではアニメーション処理のもたつきが起こります。
+アニメーション処理は端末にやらせない方が良いです。
+（端末の情報の取得の分岐はセキュリティ面の理由から却下です。）
+
+以上の理由から、代替案が見つかるまでコメントアウトします。
 */
